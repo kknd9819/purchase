22bec322d6d7be1b850755944cc580c79a5e6fa5
kknd9819 <kknd9819@qq.com>
2017-04-01 11:49:45 +0800

    整合了mybaits自动生成的一些代码

---
+++ src/main/java/top/zz/dao/PermissionDao.java
@@ -0,0 +1,30 @@
+package top.zz.dao;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import top.zz.model.Permission;
+import top.zz.model.PermissionExample;
+
+public interface PermissionDao {
+    long countByExample(PermissionExample example);
+
+    int deleteByExample(PermissionExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Permission record);
+
+    int insertSelective(Permission record);
+
+    List<Permission> selectByExample(PermissionExample example);
+
+    Permission selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Permission record, @Param("example") PermissionExample example);
+
+    int updateByExample(@Param("record") Permission record, @Param("example") PermissionExample example);
+
+    int updateByPrimaryKeySelective(Permission record);
+
+    int updateByPrimaryKey(Permission record);
+}
---
+++ src/main/java/top/zz/dao/RoleDao.java
@@ -0,0 +1,30 @@
+package top.zz.dao;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import top.zz.model.Role;
+import top.zz.model.RoleExample;
+
+public interface RoleDao {
+    long countByExample(RoleExample example);
+
+    int deleteByExample(RoleExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Role record);
+
+    int insertSelective(Role record);
+
+    List<Role> selectByExample(RoleExample example);
+
+    Role selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Role record, @Param("example") RoleExample example);
+
+    int updateByExample(@Param("record") Role record, @Param("example") RoleExample example);
+
+    int updateByPrimaryKeySelective(Role record);
+
+    int updateByPrimaryKey(Role record);
+}
